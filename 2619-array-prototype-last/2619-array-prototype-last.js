090f186512a0a1f1fa6c3f96764e1c5afe436bdd
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-04-08 23:44:29 +0530

    Time: 59 ms (9.91%), Space: 48.6 MB (68.05%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,12 +2,13 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if (this.length > 0){
-        return this[this.length-1];
-    }
-    else{
-        return -1;
-    }
+    // if (this.length > 0){
+    //     return this[this.length-1];
+    // }
+    // else{
+    //     return -1;
+    // }
+    return this.length===0 ? -1 : this[this.length-1];
 };
 
 /**
