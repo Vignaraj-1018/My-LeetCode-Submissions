b767e2778381be3d23493ad02a29eafd4cc41648
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-05-20 13:28:50 +0530

    Time: 6931 ms (5.01%), Space: 16.8 MB (32.31%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        wind = len(s1)
+        l, r = 0, wind-1
+
+        def isPermutation(s1, s2):
+            # print(s1, s2)
+            count_s1 = {}
+            count_s2 = {}
+            for i in s1:
+                if i not in count_s1:
+                    count_s1[i]=1
+                else:
+                    count_s1[i]+=1
+            
+            for i in s2:
+                if i not in count_s2:
+                    count_s2[i]=1
+                else:
+                    count_s2[i]+=1
+            
+            return count_s1 == count_s2
+        res = False
+        while r<len(s2):
+            if isPermutation(s1, s2[l:r+1]):
+                res = True
+            r+=1
+            l+=1
+        
+        return res
