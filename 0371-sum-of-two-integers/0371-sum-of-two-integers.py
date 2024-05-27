50c001dd109e1dd9f6b8a980b51e354e05088894
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-05-27 12:39:44 +0530

    Time: 41 ms (12.55%), Space: 16.6 MB (36.00%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -3,10 +3,8 @@ class Solution:
         MAX_INT = 0xFFFFFFFF
 
         while MAX_INT & b:
-            xor = a ^ b
             carry = (a & b) << 1
 
-            a = xor
+            a = a ^ b
             b = carry
-        print(a, b)
         return a & MAX_INT if b > 0 else a
