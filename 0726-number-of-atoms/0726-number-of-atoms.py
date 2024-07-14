a3af436b449bd815c433d307fb1e8f2857f0153f
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-07-14 11:34:06 +0530

    Time: 28 ms (93.59%), Space: 16.5 MB (63.66%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.py
+++ 0726-number-of-atoms/0726-number-of-atoms.py
@@ -1,39 +1,38 @@
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        def parse_formula(formula):
-            stack = [defaultdict(int)]
-            i = 0
-            while i < len(formula):
-                if formula[i] == '(':
-                    stack.append(defaultdict(int))
+        stack = [defaultdict(int)]
+        i = 0
+        while i < len(formula):
+            if formula[i] == '(':
+                stack.append(defaultdict(int))
+                i += 1
+            elif formula[i] == ')':
+                top = stack.pop()
+                i += 1
+                i_start = i
+                while i < len(formula) and formula[i].isdigit():
                     i += 1
-                elif formula[i] == ')':
-                    top = stack.pop()
+                multiplier = int(formula[i_start:i] or 1)
+                for elem, cnt in top.items():
+                    stack[-1][elem] += cnt * multiplier
+            else:
+                i_start = i
+                i += 1
+                while i < len(formula) and formula[i].islower():
                     i += 1
-                    i_start = i
-                    while i < len(formula) and formula[i].isdigit():
-                        i += 1
-                    multiplier = int(formula[i_start:i] or 1)
-                    for elem, cnt in top.items():
-                        stack[-1][elem] += cnt * multiplier
-                else:
-                    i_start = i
+                elem = formula[i_start:i]
+                i_start = i
+                while i < len(formula) and formula[i].isdigit():
                     i += 1
-                    while i < len(formula) and formula[i].islower():
-                        i += 1
-                    elem = formula[i_start:i]
-                    i_start = i
-                    while i < len(formula) and formula[i].isdigit():
-                        i += 1
-                    cnt = int(formula[i_start:i] or 1)
-                    stack[-1][elem] += cnt
-            return stack[0]
+                cnt = int(formula[i_start:i] or 1)
+                stack[-1][elem] += cnt
 
-        element_count = parse_formula(formula)
+        element_count = stack[0]
         
         res = ''
+        
         for ele in sorted(element_count):
-            res += ele
-            if element_count[ele] > 1:
-                res += str(element_count[ele])
+            count = "" if element_count[ele] == 1 else str(element_count[ele])
+            res += ele + count
+
         return res
