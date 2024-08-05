d14d0b055225ea28f134b91c96eb6f5aa879e322
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-08-05 19:13:56 +0530

    Time: 1665 ms (9.72%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT u.user_id AS buyer_id, u.join_date, SUM(CASE WHEN o.order_date LIKE '2019%' THEN 1 ELSE 0 END) AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o
+ON u.user_id = o.buyer_id
+GROUP BY u.user_id;
