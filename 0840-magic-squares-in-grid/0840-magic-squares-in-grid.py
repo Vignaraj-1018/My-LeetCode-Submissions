b7419632df0cb37971e6ba574e87538733111dbf
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-08-09 21:34:51 +0530

    Time: 40 ms (66.15%), Space: 16.6 MB (22.56%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        def helper(r, c):
+            values = set()
+            for i in range(r, r + 3):
+                for j in range(c, c + 3):
+                    if grid[i][j] in values or not (1 <= grid[i][j] <= 9):
+                        return 0
+                    values.add(grid[i][j])
+            
+            for i in range(r, r + 3):
+                if sum(grid[i][c : c + 3]) != 15:
+                    return 0
+            
+            for i in range(c, c + 3):
+                if (grid[r][i] + grid[r + 1][i] + grid[r + 2][i]) != 15:
+                    return 0
+            
+            if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2]) != 15 or (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c]) != 15:
+                return 0
+                                                    
+            return 1
+        
+        res = 0
+        for r in range(ROWS - 2):
+            for c in range(COLS - 2):
+                res += helper(r, c)
+                                                    
+        return res
