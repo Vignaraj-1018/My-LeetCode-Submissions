64f0d68f1730b56cd9ea3fcf7adb46552d1b1d0b
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-10-18 09:41:30 +0530

    Time: 245 ms (72.61%), Space: 334.1 MB (5.51%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -4,6 +4,8 @@ class Solution:
         
         for i in nums:
             max_or |= i
+            
+        cache = [[-1] * (max_or + 1) for _ in range(len(nums))]
         
         def dfs(i, cur_or):
             nonlocal max_or
@@ -11,7 +13,11 @@ class Solution:
             if i == len(nums):
                 return 1 if cur_or == max_or else 0
             
-            return dfs(i + 1, cur_or) + dfs(i + 1, cur_or | nums[i])
+            if cache[i][cur_or] != -1:
+                return cache[i][cur_or]
+            
+            cache[i][cur_or] = dfs(i + 1, cur_or) + dfs(i + 1, cur_or | nums[i])
+            return cache[i][cur_or]
             
         return dfs(0, 0)
         
