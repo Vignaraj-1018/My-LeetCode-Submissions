240c99ff3a70ae982b6396bd8910682007079fa6
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-11-02 20:50:34 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (80.88%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        for i in range(len(sentence)):
+            if sentence[i] == ' ' and sentence[i - 1] != sentence[i + 1]:
+                return False
+        return sentence[0] == sentence[-1]
