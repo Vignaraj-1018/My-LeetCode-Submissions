3f88f625a8c815dcdde0566ece1684e8c5cd7082
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-11-04 08:35:40 +0530

    Time: 167 ms (85.11%), Space: 27.3 MB (17.62%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.py
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -1,18 +1,18 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        comp = ""
+        segments = []
         cur = [0, word[0]]
-        
+
         for c in word:
-            if cur[1] == c and cur[0] < 9:
-                cur[0] += 1
-            elif cur[0] == 9:
-                comp += '9' + cur[1]
+            if c == cur[1]:
+                if cur[0] < 9:
+                    cur[0] += 1
+                else:
+                    segments.append(f"9{cur[1]}")
+                    cur = [1, c]
+            else:
+                segments.append(f"{cur[0]}{cur[1]}")
                 cur = [1, c]
-            elif cur[1] != c:
-                comp += str(cur[0]) + cur[1]
-                cur = [1, c]
-        comp += str(cur[0]) + cur[1]
-            
         
-        return comp
+        segments.append(f"{cur[0]}{cur[1]}")
+        return ''.join(segments)
