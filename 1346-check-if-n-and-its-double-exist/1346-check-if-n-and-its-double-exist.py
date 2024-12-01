a907a16caee17a777ace9ec9d4792cad9476e2b0
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-12-01 13:06:03 +0530

    Time: 0 ms (100.00%), Space: 17.3 MB (5.25%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -3,13 +3,8 @@ class Solution:
         seen = set()
         
         for n in arr:
-            if n * 2 in seen:
+            if n * 2 in seen or (n % 2 == 0 and n / 2 in seen):
                 return True
-            if n % 2 == 0:
-                if n / 2 in seen:
-                    return True
-                seen.add(n)
-            else:
-                seen.add(n)
+            seen.add(n)
                 
         return False
