bcd9fbd146b2fb635097effa79ee6b8d86e63120
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2025-01-04 09:21:39 +0530

    Time: 1560 ms, Memory: 18.3 MB - LeetHub

---
+++ 1824-81-1930-unique-length-3-palindromic-subsequences/1824-81-1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        res = set()
+        left = set()
+        right = Counter(s)
+
+        for m in s:
+            right[m] -= 1
+            for c in left:
+                if right[c] > 0:
+                    res.add((m, c))
+            left.add(m)
+
+        return len(res)
