99246b4030e551ef1116ce0234d5982585a36d1c
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2025-01-07 20:58:19 +0530

    Time: 3 ms, Memory: 17.7 MB - LeetHub

---
+++ 1256-110-1408-string-matching-in-an-array/1256-110-1408-string-matching-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res = []
+
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j:
+                   continue
+
+                if words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        
+        return res
