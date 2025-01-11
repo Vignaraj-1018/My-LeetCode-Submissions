301043ce67952c597709c38a58e223dfbfb530b7
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2025-01-11 23:06:02 +0530

    Time: 33 ms, Memory: 18.3 MB - LeetHub

---
+++ 1611-144-1400-construct-k-palindrome-strings/1611-144-1400-construct-k-palindrome-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        
+        count = Counter(s)
+        odd = 0
+        for cnt in count.values():
+            odd += cnt % 2
+        
+        return odd <= k
